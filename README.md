5dd84982ff0fc355d9e8983d4c93a02118e15a9e
narmanguebraun <narmanguebraun1@gmail.com>
2021-11-12 13:21:47 +0100

    files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mrms
+
+Playing with [Slick slider](http://github.com/kenwheeler/slick)
+
+Technologies: HTML, CSS, JavaScript, jQuery, Slick Slider.
---
+++ script.js
@@ -0,0 +1,13 @@
+// Slick Slider
+$(document).ready(function() {
+  $(".variable").slick({
+    autoplay: false,
+    autoplaySpeed: 2000,
+    speed: 1500,
+    slide: 'div',
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    variableWidth: true
+  });
+});
